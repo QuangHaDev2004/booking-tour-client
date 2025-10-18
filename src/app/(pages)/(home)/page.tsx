22477137acd52024f2f1709a922ea81de63bc992
QuangHaDev2004 <ngoquangha2004@gmail.com>
2025-10-18 10:41:47 +0700

    feat: form search section 1

--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -1,9 +1,23 @@
-import { Header } from "@/components/header/Header";
+import { FormSearch } from "./FormSearch";
 
 export default function Home() {
   return (
     <>
-      <Header />
+      <div className="bg-[url(/assets/images/bg-section-1.jpg)] bg-cover bg-center bg-no-repeat py-[107px]">
+        <div className="container">
+          <div className="mx-auto w-full lg:w-[768px]">
+            <h1 className="text-primary mb-5 text-center text-[28px] md:text-[32px] font-bold lg:text-4xl">
+              Du lịch Châu Á - Khám phá Mỹ, Úc, Âu Đi nơi đâu bạn muốn
+            </h1>
+            <p className="text-secondary mb-[30px] text-center text-sm font-normal lg:text-[16px]">
+              Trải nghiệm hành trình đáng nhớ cùng chúng tôi – khám phá văn hoá,
+              ẩm thực và cảnh đẹp khắp năm châu, mang đến kỳ nghỉ trọn vẹn cho
+              bạn và gia đình.
+            </p>
+            <FormSearch />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
