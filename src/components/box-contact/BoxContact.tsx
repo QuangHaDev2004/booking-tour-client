16d86265c44117f4bbe9d3442ba917721226aa06
QuangHaDev2004 <ngoquangha2004@gmail.com>
2025-10-22 21:11:48 +0700

    feat: box contact

---
+++ src/components/box-contact/BoxContact.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaYoutube,
+} from "react-icons/fa6";
+
+export const BoxContact = () => {
+  return (
+    <nav className="fixed right-5 bottom-6">
+      <ul
+        style={{ boxShadow: "0px 2px 4px 0px #00000026" }}
+        className="bg-primary flex flex-col items-center gap-5 rounded-[10px] p-3 text-white"
+      >
+        <li>
+          <Link href={""} target="_blank">
+            <FaFacebookF className="text-lg" />
+          </Link>
+        </li>
+        <li>
+          <Link href={""} target="_blank">
+            <FaInstagram className="text-lg" />
+          </Link>
+        </li>
+        <li>
+          <Link href={""} target="_blank">
+            <FaTwitter className="text-lg" />
+          </Link>
+        </li>
+        <li>
+          <Link href={""} target="_blank">
+            <FaYoutube className="text-lg" />
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
